99bc2c30e2a1bf54b304c0cf1ca405a931d785d2
Corey Wiley <coreytwiley@icloud.com>
2016-06-17 16:45:56 -0500

    Initial Commit

---
+++ prog1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Corey Wiley on 6/17/16.
+//  Copyright © 2016 Corey Wiley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
